a8352d19a6d0146cad914327b29b6f43892c58fb
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-19 23:23:30 -0700

    added stop reload code for form submission

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //jshint esversion:9
 
-
+document.querySelector('form').addEventListener('submit',
+    event => {
+        // Stop Reloading of Page on Form Submission
+        event.preventDefault();
+    }
+);
 
