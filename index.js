fca58095ec736326bf40c99b39a2eeb2c5895d3a
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-19 23:25:28 -0700

    add event listener for user input in form

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 //jshint esversion:9
 
+// Event Handler for Form Submission
 document.querySelector('form').addEventListener('submit',
     event => {
         // Stop Reloading of Page on Form Submission
         event.preventDefault();
+
+        // Capture user input value
+        const input = document.querySelector('input');
+
+        // Print the user input
+        console.log(input.value);
     }
 );
 
