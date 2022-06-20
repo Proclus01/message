b5892f29a572a74a349803c4fba4fa77e4961769
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-19 23:55:25 -0700

    added basic encryption mechanism for userinput

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 //jshint esversion:9
 
 // Event Handler for Form Submission
+
+// Takes user input
+// Converts user input to base64
+// Represents base64 conversion inside of secondary input field in card
+
 document.querySelector('form').addEventListener('submit',
     event => {
         // Stop Reloading of Page on Form Submission
         event.preventDefault();
 
         // Capture user input value
-        const input = document.querySelector('input');
+        const input = document.querySelector('#message-input');
 
-        // Print the user input
-        console.log(input.value);
+        // Convert input to base64 string
+        const encrypted = btoa(input.value);
+
+        // Set secondary input field to encrypted value
+        document.querySelector('#link-input').value = encrypted;
     }
 );
 
